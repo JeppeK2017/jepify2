63c4cd0e9d252226a41cb209aa39feb4264d68c7
Jeppe Kristensen <jeppe_kuk@hotmail.com>
2020-03-21 21:47:25 +0100

    Updates

---
+++ precache-manifest.27db401012636e44d43fb84aad4354ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bcef5b3ca7fa31ccacba8cae3374dc",
+    "url": "/jepifydk/index.html"
+  },
+  {
+    "revision": "d9006b01d1f077243661",
+    "url": "/jepifydk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b5971a16dcc72098b91",
+    "url": "/jepifydk/static/js/2.f4ac6ea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jepifydk/static/js/2.f4ac6ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9006b01d1f077243661",
+    "url": "/jepifydk/static/js/main.cc3fcf3f.chunk.js"
+  },
+  {
+    "revision": "d83a29151106d9fa318b",
+    "url": "/jepifydk/static/js/runtime-main.7b1f0cfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jepifydk/static/media/logo.5d5d9eef.svg"
+  }
+]);
